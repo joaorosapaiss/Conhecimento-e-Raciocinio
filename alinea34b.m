c8121e7febbc6a142c1b6053cab5a7c08a427f69
joaorosapaiss <joaorosapais@outlook.com>
2024-04-27 19:28:32 +0100

    .

--- alinea34b.m
+++ alinea34b.m
@@ -1,4 +1,4 @@
-data = readmatrix('dataset\Train_filled.csv', 'Delimiter', ';', 'DecimalSeparator', '.');
+data = readmatrix('dataset\Train_filled.csv', 'Delimiter', ',', 'DecimalSeparator', '.');
 
 % Separar os inputs e targets
 input_matrix = data(:, 1:end-1);
@@ -7,27 +7,31 @@ targets = data(:, end)';
 input_matrix = input_matrix';
 
 % Configurar para cada caso do Excel
-net = feedforwardnet([10]);
+%net = feedforwardnet([10]);
 %net = feedforwardnet([5,5]);
-%net = feedforwardnet([10,10]);
+net = feedforwardnet([10,10]);
 %net = feedforwardnet([5,10,5]);
 %net = feedforwardnet([10,10,10]);
 
 net.trainFcn = 'trainlm';
-net.layers{1}.transferFcn = 'tansig';
-net.layers{2}.transferFcn = 'purelin';
-%net.layers{3}.transferFcn = 'tansig';
-%net.layers{4}.transferFcn = 'purelin';
+net.layers{1}.transferFcn = 'logsig';
+%net.layers{2}.transferFcn = 'logsig';
+%net.layers{3}.transferFcn = 'logsig';
+net.layers{3}.transferFcn = 'purelin';
 
-net.divideParam.trainRatio = 0.75;
-net.divideParam.valRatio = 0.15;
-net.divideParam.testRatio = 0.15;
+net.divideParam.trainRatio = 0.7;
+net.divideParam.valRatio = 0.015;
+net.divideParam.testRatio = 0.015;
 
 sumGlobalAccuracy = 0;
 sumTestAccuracy = 0;
+bestGlobalAccuracy = 0;
+bestTestAccuracy = 0;
 sumTrainTime = 0;
 sumTestTime = 0;
 numberOfRuns = 10; 
+
+
 
 for k = 1:numberOfRuns 
     [net, tr] = train(net, input_matrix, targets);
@@ -35,8 +39,8 @@ for k = 1:numberOfRuns
     sumTrainTime = sumTrainTime  + trainTime;
     out = sim(net, input_matrix);
 
-    %plotconfusion(targets, out);
-    %plotperf(tr);
+    plotconfusion(targets, out);
+    plotperf(tr);
    
  
     erro = perform(net, out, targets);
@@ -60,8 +64,20 @@ for k = 1:numberOfRuns
     fprintf("Tempo de treino: %f\n", tr.best_perf);
     fprintf("Tempo de teste: %f\n", tr.best_tperf);
     fprintf('\n')
+
+    if globalAccuracy >= bestGlobalAccuracy
+        bestGlobalAccuracy = globalAccuracy;
+        bestTestAccuracy = testAccuracy;
+        bestNet = net;
+    end
+
 end
 
 fprintf('Media precisao global %.2f\n', sumGlobalAccuracy / numberOfRuns);
 fprintf('Media precisao teste %.2f\n', sumTestAccuracy / numberOfRuns);
-fprintf("Media de tempo para o treino: %.4f segundos\n", sumTrainTime / numberOfRuns);
+fprintf("Media de tempo para o treino: %f\n", sumTrainTime / numberOfRuns);
+fprintf("Media de tempo para o teste: %f\n", sumTestTime / numberOfRuns);
+
+fprintf("A Melhor precisao global %.2f\n",bestGlobalAccuracy);
+fprintf("A Melhor precisao teste %.2f\n",bestTestAccuracy);
+save('best.mat', 'bestNet');
